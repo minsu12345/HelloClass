803f26f732e57f4bf944430755da6c1ee82e1d66
minsu12345 <qnchalstn@gmail.com>
2022-01-20 13:07:22 +0900

    20200120
    193p~212p

---
+++ Assets/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal
+{
+    // Start is called before the first frame update
+
+    
+        public string name;
+    public string sound;
+
+    public void PlaySound()
+    {
+        Debug.Log(name + ":" + sound);
+    }
+    
+
+}
---
+++ Assets/Jumper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    public Rigidbody myrigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myrigidbody.AddForce(0, 500, 0);
+    }
+}
